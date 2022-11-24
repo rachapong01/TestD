2b067fa5bb6a2fcb19079ced0d9096d257195291
doy <doy.rachapong.soms@hotmail.com>
2022-11-24 16:03:14 +0700

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader';
 import ImageItem from './components/ImageItem';
 import PopUpImg from './components/PopUpImg';
 
+const dataImas = {
+  title :"2" , imgUrl:"2.jpg"
+}
+
 function App() {
+  const [selectIma,setSelectIam] = useState(null);
+  let imapost = null;
+  if (!!selectIma){
+      imapost = <PopUpImg />
+  }
   return (
     <div className="App">
       <AppHeader />
+      <p></p>
       <div className="App-grid">
         <ImageItem title="2" imgUrl="2.jpg" />
         <ImageItem title="3" imgUrl="3.jpg" />
